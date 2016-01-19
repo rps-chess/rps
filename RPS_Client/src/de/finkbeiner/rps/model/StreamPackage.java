03ab9d5ea149bf9826af0e429893872edf186c97
alex <alex-finkbeiner@gmx.de>
2016-01-19 21:01:18 +0100

    dasd

---
+++ RPS_Client/src/de/finkbeiner/rps/model/StreamPackage.java
@@ -0,0 +1,33 @@
+package de.finkbeiner.rps.model;
+
+import java.io.Serializable;
+
+public class StreamPackage implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1160186762592927659L;
+	Figure figure;
+	String message;
+	
+
+	public StreamPackage() {
+		// TODO Auto-generated constructor stub
+	}
+	public Figure getFigure() {
+		return figure;
+	}
+	public void setFigure(Figure figure) {
+		this.figure = figure;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+
+}
