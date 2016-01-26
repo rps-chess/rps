4adfdd64db7f796a7ae953aa75e3ea73310dfeb5
Florian Fay <wi14138@lehre.dhbw-stuttgart.de>
2016-01-26 21:04:06 +0100

    Anlegen von JavaProjekt für Tests und als Bibliothek

---
+++ BibliothekUndTests/src/de/fay/serialisierung/test/Objekt.java
@@ -0,0 +1,24 @@
+package de.fay.serialisierung.test;
+
+public class Objekt {
+
+	private int nummer;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNummer() {
+		return nummer;
+	}
+
+	public void setNummer(int nummer) {
+		this.nummer = nummer;
+	}
+
+}
